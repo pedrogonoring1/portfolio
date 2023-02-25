14bb9f403978ed997564232259cfd224580376f8
Pedro Gonoring <53987520+pedrogonoring1@users.noreply.github.com>
2023-02-25 11:11:46 -0300

    Criação da Pagina de Introdução

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { IntroducaoComponent } from './introducao/pages/introducao/introducao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroducaoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
